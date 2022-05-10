391dd526233dec5d5b8d5eae45727db6d1c6866f
Fas96 <firibu.anyass@gmail.com>
2022-05-11 03:23:59 +0900

    linked list

--- algo/classesPointers/classes.py
+++ algo/classesPointers/classes.py
@@ -1,24 +1,45 @@
+import uuid
 class Student:
     def __init__(self, usernmae):
         self.username = usernmae;
 
-    def get_color(self):
-        return self.username
 
-    def set_color(self, user):
-        self.username = user
+class Customer:
+    def __init__(self):
+        self.name = str.capitalize("Fas")
+        self.id = uuid.uuid4()
+
+    def set_name(self, name):
+        self.name = name
+
+    def set_id(self, cusId):
+        self.id = cusId
+
+
+class A:
+    def __init__(self):
+        print("outer class object creation")
+
+    class B:
+        def __init__(self):
+            print("inner class object creation")
+
+        def m1(self):
+            print("inner class method")
 
 
 class Cookie:
     def __init__(self, color):
         self.color = color
 
+    def get_color(self):
+        return self.color
+
+    def set_color(self, color):
+        self.color = color
+
 
 if __name__ == '__main__':
     std = Student("fas")
-    std.set_color("Bhim nation")
-
-    print(std.get_color())
-
     cookie = Cookie("Blue")
     print(cookie.color)
--- algo/classesPointers/pointers.py
+++ algo/classesPointers/pointers.py
@@ -1,4 +1,5 @@
 def pointer():
+    """Pointes of common variables does not modify in memory"""
     num = 11
     num2 = num
     print(num2, num)
@@ -7,11 +8,13 @@ def pointer():
 
 
 def dictionary():
+    """Pointers to dictionary modify the memory values if any instance of a reference is modified"""
     dic1 = {'val': 11}
     dic2 = dic1
     print(dic1, dic2)
     print("Updates when we change dictionary one: It points to same memory")
     dic1['val'] = 300
+    """Garbage collection for variables that are not used or reference at the end of execution"""
     print(dic1, dic2)
     dic3 = dic1;
     print(dic3, dic1)
