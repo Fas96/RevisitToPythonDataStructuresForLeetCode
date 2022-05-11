de822b98757f749d86b153c283db67f4abc216e9
Fas96 <firibu.anyass@gmail.com>
2022-05-11 21:40:37 +0900

    LL

--- algo/linkedList/linkedList.py
+++ algo/linkedList/linkedList.py
@@ -54,13 +54,81 @@ class LinkedList:
             self.head = new_node
             self.tail = new_node
         else:
-            """while temp is not None:
-                temp = temp.next
-            temp = new_node"""
             self.tail.next = new_node
             self.tail = new_node
         self.length += 1
         return True
+
+    def popItem(self):
+        temp = self.head
+        prev = self.head
+        if self.length == 0:
+            return None
+        temp = self.head
+        prev = self.head
+        """When more than one item exist in the linkedList"""
+        while temp.next:
+            prev = temp
+            temp = temp.next
+        self.tail = prev
+        self.tail.next = None
+        self.length -= 1
+        """If only one item exist"""
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
+
+    def prependItem(self, value):
+        new_node = Node(value)
+
+        '''If Linked list is already empty'''
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+            '''if linked list has items'''
+        self.length += 1
+        return True
+
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        """move head to next and set previous head.next(temp) to None"""
+        temp=self.head
+        self.head=self.head.next
+        temp.next=None
+        self.length-=1
+        """set tail to None when all the list is empty"""
+        if self.length==0:
+            self.tail=None
+        return temp
+
+    def get(self,index):
+        if index<0 or index>=self.length:
+            return None
+        if self.length<index:
+            temp=self.head
+            for _ in range(index):
+                temp=temp.next
+            return temp
+
+    def set_Value(self, index, value):
+        temp=self.get(index)
+        if temp:
+            temp.value=value
+            return True
+        return False
+
+
+"""        
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next"""
 
     def printList(self):
         temp = self.head
@@ -71,6 +139,13 @@ class LinkedList:
 
 if __name__ == '__main__':
     userDataList = LinkedList(3)
-    print(userDataList.head.value)
-    print(userDataList.head.next)
-    print(userDataList.tail.value)
+    userDataList.append(12)
+    userDataList.append(22)
+    userDataList.popItem()
+    userDataList.printList()
+    userDataList.popItem()
+    userDataList.printList()
+    print(userDataList.popItem())
+    userDataList.printList()
+    print(userDataList.popItem())
+    userDataList.printList()
